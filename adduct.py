74cd91f624e2d43aa5572d3af92b26ccabfaad2d
Ai Viet Hoang <extern.ai_viet.hoang@porsche.de>
2023-12-21 11:40:45 +0100

    add dynamic Hydro

--- adduct.py
+++ adduct.py
@@ -3,13 +3,20 @@ import argparse
 import sys
 import csv
 
+
 def adduct_calculation(args):
+    print("<--Begin Calculations-->")
+    for i in range(int(args.hrepeat)):
+        print("\nNumber of Hydro: %s" % (i + 1))
+        adduct_hydro(args,(i + 1))
+
+def adduct_hydro(args,number_of_hydro):
     raw_file = open(args.file, "r")
     rawdata = list(csv.reader(raw_file, delimiter=";"))
-    low_limit   = float(args.exact_mass) - float(args.mass_error)
-    high_limit  = float(args.exact_mass) + float(args.mass_error)
-    print("your low limit is    : %s" % low_limit)
-    print("your high limit is   : %s" % high_limit)
+    low_limit   = float(args.exact_mass) - float(args.mass_error) + float(number_of_hydro) * float(args.hexact)
+    high_limit  = float(args.exact_mass) + float(args.mass_error) + float(number_of_hydro) * float(args.hexact)
+    print("Low  limit after %s Hydro: %s" % (number_of_hydro,low_limit))
+    print("High limit after %s Hydro: %s" % (number_of_hydro,high_limit))
 
     list_exact_mass_of_each_element = []
     for j in range(int(args.repeat)):
@@ -26,16 +33,21 @@ def adduct_calculation(args):
             for j in rawdata:
                 if i[0][k] == float(j[1]):
                     i[0][k] = j[0]
-                    
+
+
     for i in list_add:
-        print(i)              
+        for k in i[0]:
+            plus    = k.count("+")
+            minus   = k.count("-")
+            if plus + minus - number_of_hydro == -1:
+                print( "%s,%s" % (i[0],float(i[1])))             
 
 
 def subset_sum(numbers,low_limit,high_limit,list_add,partial=[]):
     s = sum(partial)
 
     # check if the partial sum is equals to target
-    if s > low_limit and s < high_limit :
+    if s > low_limit and s < high_limit:
         #print("%s" % (partial))
         list_with_sum = [partial,s]
         list_add.append(list_with_sum)
@@ -55,10 +67,12 @@ if __name__ == "__main__":
     subparsers = main_parser.add_subparsers(help = "command help", dest = "command")
 
     parser_adduct_val = subparsers.add_parser("adduct", help = "")
-    parser_adduct_val.add_argument("file"   , help= "find the X")
-    parser_adduct_val.add_argument("exact_mass" , help="exact mass")
-    parser_adduct_val.add_argument("mass_error" , help="mass error")
-    parser_adduct_val.add_argument("repeat" , help="times of repeat elements")
+    parser_adduct_val.add_argument("file", help= "find the X")
+    parser_adduct_val.add_argument("exact_mass", help="exact mass")
+    parser_adduct_val.add_argument("mass_error", help="mass error")
+    parser_adduct_val.add_argument("repeat", help="times of repeat elements")
+    parser_adduct_val.add_argument("hrepeat", help="number of Hydro repeat")
+    parser_adduct_val.add_argument("hexact",  help="exact mass number of Hydro")
     args = main_parser.parse_args(sys.argv[1:])
     
     if args.command == "adduct":
