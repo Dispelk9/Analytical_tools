ff62d6b1dbd12b2ed5a446bc3aa126bb3a07ca5f
Ai Viet Hoang <extern.ai_viet.hoang@porsche.de>
2024-09-05 13:22:56 +0200

    gunicorn with Apache2 port 8080

---
+++ gunicorn_config.py
@@ -0,0 +1,23 @@
+#pip install gunicorn
+#for http
+#gunicorn --config gunicorn_config.py adduct_flask:app
+#for https
+#gunicorn --config gunicorn_config.py adduct_flask:app --certfile=/etc/letsencrypt/live/analytical.dispelk9.de/fullchain.pem --keyfile=/etc/letsencrypt/l
+ive/analytical.dispelk9.de/privkey.pem
+import os
+
+
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+
+
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
