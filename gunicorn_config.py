39a4882f0a4bf43d32cc1fd9dfceea14cf744e84
Ai Viet Hoang <extern.ai_viet.hoang@porsche.de>
2024-10-25 09:44:41 +0200

    update comment reboot

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,6 +2,7 @@
 #for http
 #gunicorn --config gunicorn_config.py adduct_flask:app
 #for https
+#run this command after reboot
 #gunicorn --config gunicorn_config.py adduct_flask:app --certfile=/etc/letsencrypt/live/analytical.dispelk9.de/fullchain.pem --keyfile=/etc/letsencrypt/live/analytical.dispelk9.de/privkey.pem > /tmp/flask_log.txt 2>&1 &
 import os
 
